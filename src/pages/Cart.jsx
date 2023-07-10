dfb1eddb26f4adfab14f7abae7df7c205e302b5c
soni895 <darshansoni895@gmail.com>
2023-07-10 18:35:22 +0530

    cart created

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,25 @@
+import { useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
+import CartItem from "../components/CartItem";
 
 const Cart = () => {
+  const {cart}=useSelector((state)=>state);
   return <div>
-cart
+  {
+    cart.length>0?(
+      Cart.map(
+           (item)=>(<CartItem item={item} key={item.id} index={item.index}></CartItem>)
+      )
+      ):(
+      <div>
+        <h2>Your Cart Is Empty Cart</h2>
+        <NavLink to="/"> Shop Now</NavLink>
+      </div>
+       
+       
+  
+    )
+  }
   </div>;
 };
 
