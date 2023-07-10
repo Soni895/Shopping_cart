f4c4824a195d8361625b4cb7932bbccf8fb56854
soni895 <darshansoni895@gmail.com>
2023-07-10 10:46:51 +0530

    routes created

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar'
 import Home from './pages/Home';
+import Cart from "./pages/Cart"
 
 const App = () => {
   
   return (
     <div>
       <Navbar></Navbar>
-      
-      <Home></Home>
+      <Routes>
+      <Route path="/" element={<Home></Home>}/>
+      <Route path="/cart" element={<Cart></Cart>}/>
+      </Routes>
     </div>
   );
 };
