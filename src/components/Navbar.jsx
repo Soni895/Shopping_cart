9125fc41ae83c20ef5b3ee2b737f8165db2f5e84
soni895 <darshansoni895@gmail.com>
2023-07-10 11:09:40 +0530

    navlink added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,15 @@
+import  { AiOutlineShoppingCart } from 'react-icons/ai';
+import { NavLink } from 'react-router-dom';
+// import img from "../../public/android-chrome-512x512.png"
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return <div className='flex justify-between'>
+    
+ <div >
+ <NavLink to="./" >  <img src="../../public/android-chrome-512x512.png"></img></NavLink>
+ </div>   
+ <NavLink to="./"><div>Home</div></NavLink>
+<NavLink to="./cart"> <div><AiOutlineShoppingCart></AiOutlineShoppingCart></div></NavLink>
+  </div>;
 };
 
 export default Navbar;
