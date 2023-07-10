cfdba168f3ec9136f938f36fbbe1b72ec23a99a2
soni895 <darshansoni895@gmail.com>
2023-07-10 19:04:38 +0530

    cart item completed

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,8 +1,26 @@
+import { AiTwotoneDelete } from 'react-icons/ai';
 const CartItem = ({item}) => {
   return <div>
+  <div><img src={item.image}></img></div>
+  <div>
+  <p> {item.title}</p>
+  <p>{item.description}</p>
+  <div>
+  <p>{item.price}</p>
+  <button>
+     <AiTwotoneDelete/>
+  </button>
+  </div>
+  </div>
+  
 
-    
-  </div>;
+  
+
+
+  
+  
+    </div>;
+  
 };
 
 export default CartItem;
